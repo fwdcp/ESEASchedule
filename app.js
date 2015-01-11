f5e93b144263282b66b1bc0a9acfb4a21184af70
thesupremecommander <thesupremecmdr@gmail.com>
2015-01-11 14:45:07 -0500

    Add match retrieval.

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ var request = require('request');
 var underscore = require('underscore');
 var async = require('async');
 var moment = require('moment');
+var Cacheman = require('cacheman');
 
 var config = require('./config');
 var express = require('./express');
 
 var jar = request.jar();
 jar.setCookie(request.cookie('viewed_welcome_page=1'), 'http://play.esea.net');
+
+var scheduleCache = new Cacheman('schedule');
 
 express.get('/filters.json', function(req, res) {
     async.auto({
@@ -113,7 +116,7 @@ express.get('/filters.json', function(req, res) {
             });
         }],
         "currentWeek": function(cb) {
-            cb(null, {'weekBegin': moment().day(0), 'weekEnd': moment().day(6), 'nextWeekEnd': moment().day(14)});
+            cb(null, {'weekBegin': moment().day(0).startOf('day'), 'weekEnd': moment().day(6).endOf('day'), 'nextWeekEnd': moment().day(14).startOf('day')});
         }
     }, function(err, results) {
         if (err) {
@@ -122,6 +125,147 @@ express.get('/filters.json', function(req, res) {
         }
         else {
             res.json(underscore.extend({}, results.schedule, results.lowestDivision, results.highestDivision, results.currentWeek));
+        }
+    });
+});
+
+express.get('/matches/:start/:end/list.json', function(req, res) {
+    async.auto({
+        "dates": function(cb) {
+            var dates = [];
+            var currentDate = moment(req.params.start);
+            var endDate = moment(req.params.end);
+
+            while (currentDate <= endDate) {
+                dates.push(currentDate.format('YYYY-MM-DD'));
+                currentDate.add(1, 'days');
+            }
+
+            cb(null, dates);
+        },
+        "mostRecentDivision": function(cb) {
+            request({
+                uri: 'http://play.esea.net/index.php',
+                qs: {
+                    's': 'league',
+                    'd': 'standings',
+                    'format': 'json'
+                },
+                json: true,
+                jar: jar
+            }, function(err, http, body) {
+                if (err || http.statusCode != 200) {
+                    cb(err || http.statusCode);
+                }
+                else {
+                    var divisionIDs = underscore.flatten(underscore.map(underscore.flatten(underscore.map(underscore.values(body.select_division_id), underscore.values)), underscore.keys));
+
+                    cb(null, underscore.max(divisionIDs, Number));
+                }
+            });
+        },
+        "seasonDates": ['mostRecentDivision', function(cb, results) {
+            request({
+                uri: 'http://play.esea.net/index.php',
+                qs: {
+                    's': 'league',
+                    'd': 'standings',
+                    'division_id': results.mostRecentDivision,
+                    'format': 'json'
+                },
+                json: true,
+                jar: jar
+            }, function(err, http, body) {
+                if (err || http.statusCode != 200) {
+                    cb(err || http.statusCode);
+                }
+                else {
+                    cb(null, {'start': moment.unix(body.division.time_start), 'end': moment.unix(body.division.time_end)});
+                }
+            });
+        }],
+        "matches": ['dates', 'seasonDates', function(cb, results) {
+            async.map(results.dates, function(date, cb) {
+                scheduleCache.get(item, function(err, info) {
+                    if (err) {
+                        cb(err);
+                    }
+                    else {
+                        if (!info) {
+                            request({
+                                uri: 'http://play.esea.net/index.php',
+                                qs: {
+                                    's': 'league',
+                                    'd': 'schedule',
+                                    'date': date,
+                                    'region_id': 'all',
+                                    'game_id': 'all',
+                                    'division_level': 'all',
+                                    'format': 'json'
+                                },
+                                json: true,
+                                jar: jar
+                            }, function(err, http, body) {
+                                if (err || http.statusCode != 200) {
+                                    cb(err || http.statusCode);
+                                }
+                                else {
+                                    // CACHE EXPIRY:
+                                    // today: 15 sec
+                                    // yesterday/tomorrow: 5 minutes
+                                    // this week: 30 minutes
+                                    // last/next week: 1 hour
+                                    // remainder of season: 3 hours
+                                    // everything else: 24 hours
+
+                                    var ttl;
+
+                                    if (moment(date).isSame(moment(), 'day')) {
+                                        ttl = '15s';
+                                    }
+                                    else if (moment(date).isBetween(moment().subtract(1, 'days'), moment().add(1, 'days'), 'day')) {
+                                        ttl = '5m';
+                                    }
+                                    else if (moment(date).isBetween(moment().day(0), moment().day(6), 'day')) {
+                                        ttl = '30m';
+                                    }
+                                    else if (moment(date).isBetween(moment().day(-7), moment().day(13), 'day')) {
+                                        ttl = '1h';
+                                    }
+                                    else if (moment(date).isBetween(moment(results.seasonDates.start), moment(results.seasonDates.end), 'day')) {
+                                        ttl = '3h';
+                                    }
+                                    else {
+                                        ttl = '24h';
+                                    }
+
+                                    schedule.set(date, body, ttl, function(err, info) {
+                                        cb(null, underscore.values(info.matches));
+                                    });
+                                }
+                            });
+                        }
+                        else {
+                            cb(null, underscore.values(info.matches));
+                        }
+                    }
+                });
+            }, function(err, results) {
+                if (err) {
+                    cb(err);
+                }
+                else {
+                    cb(null, underscore.flatten(results.matches));
+                }
+            });
+        }]
+    }, function(err, results) {
+        if (err) {
+            console.log(err);
+            res.status(500).end();
+        }
+        else {
+            res.json(results.matches);
         }
     });
 });
